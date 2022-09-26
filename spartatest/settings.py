1968ac2167a703b7f33e333a245cc7a1f112d6ec
yoonsun9128 <yoonsun9128@gmail.com>
2022-09-26 10:44:35 +0900

    setting apps change

--- spartatest/settings.py
+++ spartatest/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'introduce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'introduce',
 ]
 
 MIDDLEWARE = [
