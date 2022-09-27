9e5b0a0827b316696de099dfd6065d124be26e4f
yoonsun9128 <yoonsun9128@gmail.com>
2022-09-27 09:33:11 +0900

    models accesslog add

--- introduce/models.py
+++ introduce/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+class Accesslogs(models.Model): 
+    class Meta:
+        db_table = "my_user"
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=20, null=False)
