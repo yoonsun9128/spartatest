468874f511e6cbab756d52662e1fe4d4f63874e8
yoonsun9128 <yoonsun9128@gmail.com>
2022-09-27 17:41:49 +0900

    access_log add

--- introduce/models.py
+++ introduce/models.py
@@ -6,3 +6,9 @@ class Accesslogs(models.Model):
     
     created_at = models.DateTimeField("접속 시간", auto_now_add=True)
     location = models.CharField("접속 경로", max_length=50)
+
+    def __str__(self):
+        #내가 쓰고 싶은 텍스트를 안에다가 넣어주면 된다f"{내가쓰고 싶은 텍스트}"
+        return f"{self.created_at} / {self.location}"
+
+
