2f0479c4a4f80773f2e35686ac86c7bb8bb6b549
yoonsun9128 <yoonsun9128@gmail.com>
2022-09-27 17:26:34 +0900

    log time check

--- introduce/views.py
+++ introduce/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from introduce.models import Accesslogs
 
 # Create your views here.
 def intro(request):
+    access_log = Accesslogs()
+    # 지정 어디 페이지를 들어갈때 로그할껀지 지정
+    access_log.location = "introduce"
+    access_log.save()
     return render(request, 'intro.html')
