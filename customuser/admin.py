1f9e1ec6187c3cbf1708f330f5c5ed29243ef56b
yoonsun9128 <yoonsun9128@gmail.com>
2022-09-28 11:08:35 +0900

    custom_user save in database

--- customuser/admin.py
+++ customuser/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from customuser.models import CustomModel
 # Register your models here.
+admin.site.register(CustomModel)
--- customuser/models.py
+++ customuser/models.py
@@ -1,11 +1,11 @@
-from unittest.util import _MAX_LENGTH
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomModel(AbstractUser):
-        class Meta:
-            db_table = "custom_user"
+    class Meta:
+        db_table = "custom_user"
 
-        address = models.TextField(max_length=500, blank=True)
-        bio = models.TextField(max_length=500, blank=True)
+    address = models.TextField(max_length=500, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
